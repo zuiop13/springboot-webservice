a6973560053fd56dcf92041024051ca457f227f7
zuiop13 <zuiop13@naver.com>
2020-04-17 19:32:06 +0900

    CI 구성

--- src/test/java/com/shjeong/admin/web/IndexControllerTest.java
+++ src/test/java/com/shjeong/admin/web/IndexControllerTest.java
@@ -15,12 +15,21 @@ public class IndexControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
-/*    @Test
+//    @Test
+//    public void 메인페이지_로딩() {
+//        //when
+//        String body = this.restTemplate.getForObject("/",String.class);
+//
+//        //then
+//        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+//    }
+
+    @Test
     public void 메인페이지_로딩() {
         //when
-        String body = this.restTemplate.getForObject("/",String.class);
+        String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
-    }*/
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
+    }
 }
