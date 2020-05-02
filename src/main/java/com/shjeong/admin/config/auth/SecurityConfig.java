1a23b1073b6252fe4e58d1526039d3b6ac05baf3
zuiop13 <zuiop13@naver.com>
2020-05-02 12:04:39 +0900

    profile 추가 로직

--- src/main/java/com/shjeong/admin/config/auth/SecurityConfig.java
+++ src/main/java/com/shjeong/admin/config/auth/SecurityConfig.java
@@ -1,33 +1,34 @@
 package com.shjeong.admin.config.auth;
 
-import com.shjeong.admin.domain.user.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import com.shjeong.admin.domain.user.Role;
 
 @RequiredArgsConstructor
-@EnableWebSecurity
+@EnableWebSecurity  // 1. Spring Security 설정들을 활성화 시켜줌
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
     private final CustomOAuth2UserService customOAuth2UserService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .headers().frameOptions().disable()
+                .headers().frameOptions().disable() // 2. h2-console 화면을 사용하기 위해 해당 옵션들을 disable 함
                 .and()
-                    .authorizeRequests()
-                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
-                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
-                    .anyRequest().authenticated()
+                .authorizeRequests()    // 3. URL별 권한 관리를 설정하는 옵션의 시작점 & authorizeRequests가 선언되어야만 antMatchers 옵션을 사용할 수 있음
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())    // 4. 권한 관리 대상을 지정하는 옵션 & URL, HTTP 메소드별로 관리가 가능함 & "/"등 지정된 URL들은 permitAll() 옵션ㅇ르 통해 전체 열람 권한을 주었음 & POST 메소드이면서 "/api/v1/**" 주소를 가진 API는 USER 권한을 가진 사람만 가능하도록 구현
+                .anyRequest().authenticated()   // 5. 설정된 값들 이외 나머지 URL들을 나타냄 & 여기서느 authenticated()을 추가하여 나머지 URL들은 모두 인증된 사
                 .and()
-                    .logout()
-                        .logoutSuccessUrl("/")
+                .logout()
+                .logoutSuccessUrl("/")  // 6. 로그아웃 기능에 대한 여러 설정의 진입점 & 로그아웃 성공시 / 주소로 이동
                 .and()
-                    .oauth2Login()
-                        .userInfoEndpoint()
-                            .userService(customOAuth2UserService);
+                .oauth2Login()  // 7. OAuth2 로그인 기능에 대한 여러 설정의 진입점
+                .userInfoEndpoint() // 8. OAuth 2 로그인 성공 이후 사용자 정보를 가져올 때의 설정들을 담당합니다.
+                .userService(customOAuth2UserService);  // 9. 소셜 로그인 성공 시 후속 조치를 진행할 UserService 인터페이스의 ㅅ구현체를 등록합니다. & 리소스 서버(즉, 소셜 서비스들)에서 사용자 정보를 가졍온 상태에서 추가로 진행하고자 하는 기능을 명시할 수 있습니다.
+
     }
 }
+
